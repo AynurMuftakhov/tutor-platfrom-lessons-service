3022541078133f7a89a58a1a7686017a9b55e89f
AynurMuftakhov <aynur2394@gmail.com>
2025-03-15 18:00:06 +0500

    disable security, fix app start

--- src/main/java/com/mytutorplatform/lessonsservice/LessonsServiceApplication.java
+++ src/main/java/com/mytutorplatform/lessonsservice/LessonsServiceApplication.java
@@ -1,4 +1,4 @@
-package com.mytutorplatform.lessonsservice.application;
+package com.mytutorplatform.lessonsservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/mytutorplatform/lessonsservice/controller/LessonController.java
+++ src/main/java/com/mytutorplatform/lessonsservice/controller/LessonController.java
@@ -9,7 +9,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/lessons")
+@RequestMapping("/api/lessons")
 @SuppressWarnings("unused")
 public class LessonController {
 
